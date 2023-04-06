3357b8831e3adbea2d668952c5224117498e2d30
Nafi R <saejim91@gmail.com>
2023-04-07 00:00:34 +0600

    [0]

--- gitIt.ps1
+++ gitIt.ps1
@@ -1,10 +1,21 @@
-function gitIt{
+# function gitIt{
 
+#    git status
+#    git add -A
+#    git commit -m "$args[0]"
+#    git push
+# }
+
+
+# gitIt "default powerShell commit" 
+
+function gitIt {
    git status
    git add -A
-   git commit -m "$args[0]"
+   # Prompt the user to enter a commit message
+   $message = Read-Host "Enter a commit message"
+   git commit -m "$message"
    git push
 }
 
-
-gitIt "default powerShell commit" 
+gitIt
