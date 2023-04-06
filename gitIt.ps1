2e18b7d3498587f436c29a8aef703f3521ddf12d
Nafi R <saejim91@gmail.com>
2023-04-06 23:54:05 +0600

    [0]

--- gitIt.ps1
+++ gitIt.ps1
@@ -7,4 +7,19 @@ function gitIt{
 }
 
 
-gitIt "default powerShell commit" 
+gitIt [ArgumentCompleter({
+   [OutputType([System.Management.Automation.CompletionResult])]
+   param(
+      [string] $CommandName,
+      [string] $ParameterName,
+      [string] $WordToComplete,
+      [System.Management.Automation.Language.CommandAst] $CommandAst,
+      [System.Collections.IDictionary] $FakeBoundParameters
+   )
+   
+   $CompletionResults = [System.Collections.Generic.List[System.Management.Automation.CompletionResult]]::new()
+   
+   "default powerShell commit" 
+   
+   return $CompletionResults
+})]
